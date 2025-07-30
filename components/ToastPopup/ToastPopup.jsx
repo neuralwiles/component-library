b42e8d4a03f2192591b90a41f918ea9c2eacd2e6
neuralwiles <thevengefulpolyglot@gmail.com>
2025-07-30 09:03:34 -0700

    Fix for toast not fading out gradually

--- components/ToastPopup/ToastPopup.jsx
+++ components/ToastPopup/ToastPopup.jsx
@@ -3,27 +3,7 @@ import { createPortal } from "react-dom";
 import { ToastPopupContext } from "./ToastPopupContainer";
 
 export default function ToastPopup({ children }) {
-  const { type, open } = React.useContext(ToastPopupContext);
-  const [visible, setVisible] = React.useState(false);
-
-  React.useEffect(() => {
-    if (open) {
-      setVisible(true);
-    } else {
-      setVisible(false);
-
-      // Previously I tried adding a setTimeout for setting visible to false and then calling hideToast (which I passed down through the context) after another delay, but that doesn't work for ??? reasons.
-      // I think it also might call hideToast() on every render if it's a dependency... Maybe I need to wrap in callback function?
-
-      /*setTimeout(() => {
-        setVisible(false);
-      }, 3000);
-
-      setTimeout(() => {
-        hideToast();
-      }, 500);*/
-    }
-  }, [open]);
+  const { type, open, visible } = React.useContext(ToastPopupContext);
 
   return open
     ? createPortal(
--- components/ToastPopup/ToastPopupContainer.jsx
+++ components/ToastPopup/ToastPopupContainer.jsx
@@ -5,12 +5,14 @@ export { ToastPopupContext };
 
 export default function ToastPopupContainer({ children, type = "neutral" }) {
   const [open, setOpen] = React.useState(false);
+  const [visible, setVisible] = React.useState(false);
 
   function showToast() {
     setOpen(true);
 
-    // The problem is that the following sets open to false after the delay, which immediately removes the element from the DOM, not allowing for any chance for the transition.
-    // I tried moving the visible state and useEffect expression up to this component to see if that would fix it, but it didn't. At least not the way I did it anyway.
+    setTimeout(() => {
+      setVisible(true);
+    }, 100);
 
     setTimeout(() => {
       hideToast();
@@ -18,11 +20,15 @@ export default function ToastPopupContainer({ children, type = "neutral" }) {
   }
 
   function hideToast() {
-    setOpen(false);
+    setVisible(false);
+
+    setTimeout(() => {
+      setOpen(false);
+    }, 500);
   }
 
   return (
-    <ToastPopupContext.Provider value={{ type, open, showToast, hideToast }}>
+    <ToastPopupContext.Provider value={{ type, open, visible, showToast }}>
       <div className="toast-popup-container">{children}</div>
     </ToastPopupContext.Provider>
   );
